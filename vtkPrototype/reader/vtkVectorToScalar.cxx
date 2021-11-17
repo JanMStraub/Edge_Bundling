c40e110596fe8e5bffff31a8fbaa80a82d653ddc
Jan Straub <Straub.JanM@gmail.com>
2021-11-17 08:58:29 +0100

    code cleanup

--- vtkPrototype/reader/vtkVectorToScalar.cxx
+++ vtkPrototype/reader/vtkVectorToScalar.cxx
@@ -15,19 +15,17 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
 #include <vtkVertexGlyphFilter.h>
-#include <vtkColorTransferFunction.h>
-
 
 int main () {
-    // simply set filename here (oh static joy)
+
+    std::cout << "Program start" << std::endl;
+
     std::string inputFilename = "/Users/jan/Google Drive/Programmieren/bachelor_thesis/vtkPrototype/data/tmp_3D.vtk";
 
-    // Get all data from the file
-    vtkSmartPointer<vtkUnstructuredGridReader> reader = vtkSmartPointer<vtkUnstructuredGridReader>::New();
+    vtkNew<vtkUnstructuredGridReader> reader;
     reader->SetFileName(inputFilename.c_str());
     reader->Update();
-    
-    std::cout << "Test" << std::endl;
+
     vtkUnstructuredGrid* output = reader->GetOutput();
 
     int numberOfPoints = output->GetNumberOfPoints();
@@ -42,43 +40,23 @@ int main () {
     poly->DeepCopy(output);
     poly->GetPointData()->SetScalars(scalars);
 
-    vtkSmartPointer<vtkVertexGlyphFilter> vertexGlyphFilter = vtkSmartPointer<vtkVertexGlyphFilter>::New();
+    vtkNew<vtkVertexGlyphFilter> vertexGlyphFilter;
     vertexGlyphFilter->SetInputData(poly);
 
-    vtkSmartPointer<vtkPolyDataMapper> signedDistanceMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    vtkNew<vtkPolyDataMapper> signedDistanceMapper;
     signedDistanceMapper->SetInputConnection(vertexGlyphFilter->GetOutputPort() );
     signedDistanceMapper->ScalarVisibilityOn();
 
-    double colors[5][3] = { { 0, 0, 1},
-                            { 0, 0.5, 0.5 },
-                            { 0, 1, 0 },
-                            { 0.5, 0.5, 0 },
-                            { 1, 0, 0 } };
-    vtkSmartPointer<vtkColorTransferFunction> colorFunction =
-            vtkSmartPointer<vtkColorTransferFunction>::New();
-    colorFunction->SetUseBelowRangeColor( true );
-    colorFunction->SetBelowRangeColor( colors[0] );
-    colorFunction->SetNanColor( colors[0] );
-    colorFunction->AddRGBPoint( -0.4, colors[0][0], colors[0][1], colors[0][2] );
-    colorFunction->AddRGBPoint( 0, colors[1][0], colors[1][1], colors[1][2] );
-    colorFunction->AddRGBPoint( 0.6, colors[2][0], colors[2][1], colors[2][2] );
-    colorFunction->AddRGBPoint( 1.5, colors[3][0], colors[3][1], colors[3][2] );
-    colorFunction->AddRGBPoint( 2.9, colors[4][0], colors[4][1], colors[4][2] );
-    colorFunction->Build();
-
-    signedDistanceMapper->SetLookupTable(colorFunction);
-    signedDistanceMapper->UseLookupTableScalarRangeOn();
-
-    vtkSmartPointer<vtkActor> signedDistanceActor = vtkSmartPointer<vtkActor>::New();
+    vtkNew<vtkActor> signedDistanceActor;
     signedDistanceActor->SetMapper(signedDistanceMapper);
 
-    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    vtkNew<vtkRenderer> renderer;
     renderer->AddViewProp(signedDistanceActor);
 
-    vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+    vtkNew<vtkRenderWindow> renderWindow;
     renderWindow->AddRenderer(renderer);
 
-    vtkSmartPointer<vtkRenderWindowInteractor> renWinInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    vtkNew<vtkRenderWindowInteractor> renWinInteractor;
     renWinInteractor->SetRenderWindow(renderWindow);
 
     renderWindow->Render();
